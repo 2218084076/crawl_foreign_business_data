a20eafcc54fe9770337f82ddb78266f74a318efa
Terry <82017869+2218084076@users.noreply.github.com>
2022-07-11 17:49:50 +0800

    test: russian spider test

---
+++ test/test_russia_spider.py
@@ -0,0 +1,49 @@
+import pytest
+from scrapy import Selector
+
+from spiders.russia_spider import extract_russian_company_links, RussiaCrawlIndex, ParseRussiaCompanyInfo
+
+
+def test_extract_russian_links():
+    with open('data/russia_page.html', 'r', encoding='utf-8') as f:
+        response = Selector(text=f.read())
+        url = 'https://www.rusprofile.ru/ip/М'
+
+        result = extract_russian_company_links(url, response)
+        assert result == (['https://www.rusprofile.ru/ip/305233215000190'], ['https://www.rusprofile.ru/ip/М'])
+
+
+@pytest.mark.parametrize(
+    'url',
+    [
+        "ip/A",
+        "test",
+        None,
+        "https://www.rusprofile.ru/TEST"
+    ]
+)
+def test_russia_crawl_index(url):
+    page_text = f'''
+<html lang="ru">
+    <ul class="letter-list">
+    <li><a href="{url}">А</a></li>
+    </ul>
+</html>
+    '''
+    response = Selector(text=page_text)
+
+    if url == "ip/A":
+        result = RussiaCrawlIndex().parse(response)
+        assert result == {'index_list': ['https://www.rusprofile.ru/%s' % url]}
+
+    else:
+        result = RussiaCrawlIndex().parse(response)
+        assert result == {'index_list': []}
+
+
+def test_parse_russia_company_info(mocker):
+    with open('data/ruaaia_company_info.html', 'r', encoding='utf-8') as f:
+        response = mocker.MagicMock(text=f.read())
+        result = ParseRussiaCompanyInfo().parse(response)
+
+        assert result.get('russia_company_infos').get('OGRNIP') == '319508100170320'
